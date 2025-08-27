b024cddcf17212a30efaaa967d71536b2ba04517
Deepthi <mmine3030@gmail.com>
2025-08-27 15:41:46 +0530

    upadated2

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,21 +1,29 @@
-import { Fragment ,useEffect} from "react";
-import { useDispatch } from "react-redux";
-import { fetchRandomRecipes } from "../store/searchSlice"
-import ContentHeading from "./ContentHeading"
+import { Fragment, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchRandomRecipes } from "../store/searchSlice";
+import ContentHeading from "./ContentHeading";
 import styles from "./Content.module.css";
 import Items from "./Items";
-function Content(){
-    const dispatch = useDispatch(); 
-    useEffect(() => {
-        dispatch(fetchRandomRecipes());
-                }, [dispatch]);
-    return (<Fragment>
-        <div className={styles.body}>
-            <ContentHeading/>
 
-            </div>
-        <Items/>
+function Content() {
+  const dispatch = useDispatch();
+  const { items } = useSelector((state) => state.recipes);
 
-    </Fragment>)
+  useEffect(() => {
+    // Fetch only if items are empty (first load)
+    if (items.length === 0) {
+      dispatch(fetchRandomRecipes());
+    }
+  }, [dispatch, items.length]);
+
+  return (
+    <Fragment>
+      <div className={styles.body}>
+        <ContentHeading />
+      </div>
+      <Items />
+    </Fragment>
+  );
 }
+
 export default Content;
